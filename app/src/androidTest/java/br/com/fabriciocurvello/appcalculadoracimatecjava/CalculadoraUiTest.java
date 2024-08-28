2e5597b084d8b922c82482b6a82222354c755f02
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2024-08-27 23:20:21 -0300

    Teste Ui AdicaoTestUi

--- app/src/androidTest/java/br/com/fabriciocurvello/appcalculadoracimatecjava/CalculadoraUiTest.java
+++ app/src/androidTest/java/br/com/fabriciocurvello/appcalculadoracimatecjava/CalculadoraUiTest.java
@@ -5,12 +5,11 @@ import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
 
-public class ClaculadoraUiTest {
+public class CalculadoraUiTest {
 
     @Rule
     public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
