9b1e3169c9f6afa907be116733c910bb8b8c2e44
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2024-08-27 22:19:33 -0300

    Teste Unitário adicaoTest()

---
+++ app/src/test/java/br/com/fabriciocurvello/appcalculadoracimatecjava/CalculadoraTest.java
@@ -0,0 +1,14 @@
+package br.com.fabriciocurvello.appcalculadoracimatecjava;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculadoraTest {
+
+    @Test
+    public void adicaoTest() {
+        Calculadora calculadora = new Calculadora();
+        Double resultado = calculadora.calcular(2.0, 3.0, "Adição");
+        Assert.assertEquals(5.0,resultado, 0.01);
+    }
+}
